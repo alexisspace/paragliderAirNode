776a70dbdbaddf0360c9a5d729b2d0075fe4594d
alexisspace <alexis.guillermo.ortega.acurero@gmail.com>
2016-09-05 19:48:09 -0400

    1 st test commit

---
+++ myApp5.X/build/default/production/_ext/1688732426/insgps16state_v1.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1688732426/insgps16state_v1.o:  \
+ ../src/system_config/default/insgps16state_v1.c  \
+ ../src/system_config/default/insgps.h 
---
+++ myApp5.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=myApp5.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/myApp5.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=myapp5.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/myapp5.x.tar
